7b64fb6dacdba50b471472616a2451f270155cd7
aliazeemabbas <aliazeem480@gmail.com>
2023-09-05 17:55:03 +0500

    added env file

--- db/connect.js
+++ db/connect.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose')
 
-const connectionString = 
-'mongodb+srv://admin:Aaliyan1@task-manager.ps7dpzz.mongodb.net/Task-Manager?retryWrites=true&w=majority'
-
 const connectDB = (url) => {
-    return mongoose.connect(connectionString)
+    return mongoose.connect(url)
 }
 
 module.exports = connectDB
