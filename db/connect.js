0b259094c81a4ccc695810012d3a20017cc71ff3
aliazeemabbas <aliazeem480@gmail.com>
2023-09-05 17:48:03 +0500

    updated db connection

--- db/connect.js
+++ db/connect.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose')
 const connectionString = 
 'mongodb+srv://admin:Aaliyan1@task-manager.ps7dpzz.mongodb.net/Task-Manager?retryWrites=true&w=majority'
 
-mongoose
-    .connect(connectionString)
-    .then(() => console.log('Connected to DB'))
-    .catch((err) => console.log(err))
+const connectDB = (url) => {
+    return mongoose.connect(connectionString)
+}
+
+module.exports = connectDB
+    
