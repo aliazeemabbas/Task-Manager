fd85cdde93c85218a38ea76a6a3365c43c43a102
aliazeemabbas <aliazeem480@gmail.com>
2023-09-06 09:18:17 +0500

    updated getTask API

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -16,7 +16,6 @@ const createTask = async (req, res) => {
     } catch (error) {
         res.status(500).json({ msg: error })
     }
-    
 }
 
 // const createTask = async (req, res) => {
@@ -25,8 +24,17 @@ const createTask = async (req, res) => {
 //     res.status(201).json({ task })
 // }
 
-const getTask = (req, res) => {
-    res.json({id: req.params.id})
+const getTask = async (req, res) => {
+    try {
+        const {id: taskID} = req.params
+        const task = await Task.findOne({_id:taskID});
+        if(!task){
+            return res.status(404).json({msg: `No task with id: ${taskID}`})
+        }
+        res.status(200).json({task})
+    } catch (error) {
+        res.status(500).json({ msg: error })
+    }
 }
 
 const updateTask = (req, res) => {
