7a60329041ca68b327621202930094407a74902a
aliazeemabbas <aliazeem480@gmail.com>
2023-09-06 08:32:30 +0500

    updated createTask API

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -4,8 +4,14 @@ const getAllTasks = (req, res) => {
     res.send('get all tasks')
 }
 
-const createTask = (req, res) => {
-    res.json(req.body)
+// const createTask = (req, res) => {
+//     res.json(req.body)
+// }
+
+const createTask = async (req, res) => {
+    const task = new Task(req.body);
+    await task.save();
+    res.status(201).json({ task })
 }
 
 const getTask = (req, res) => {
