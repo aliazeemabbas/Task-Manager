05ebb0cd67cdaed8303efa888fa33c840ca5299c
aliazeemabbas <aliazeem480@gmail.com>
2023-09-08 08:14:14 +0500

    added error handler

---
+++ middleware/error-handler.js
@@ -0,0 +1,6 @@
+const errorHandleMiddleware = (err, req, res, next) => {
+    console.log(err)
+    return res.status(err.status).json({msg: err.message})
+}
+
+module.exports = errorHandleMiddleware
