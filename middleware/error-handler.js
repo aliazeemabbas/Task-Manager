133df6e3077abd1ccdf09b2c482f1fece7b79111
aliazeemabbas <aliazeem480@gmail.com>
2023-09-08 08:33:53 +0500

    added custom error class

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,6 +1,10 @@
+const { CustomAPIError } = require('../errors/custom-error')
+
 const errorHandleMiddleware = (err, req, res, next) => {
-    console.log(err)
-    return res.status(err.status).json({msg: err.message})
+    if(err instanceof CustomAPIError){
+        return res.status(err.statusCode).json({msg: err.message})
+    }
+    return res.status(500).json({msg: 'Something went wrong, try again later'})
 }
 
 module.exports = errorHandleMiddleware
